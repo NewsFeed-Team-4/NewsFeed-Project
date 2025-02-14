a1eb1c6b0eb9d23e15769f68dee7f60c1240a8bf
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-02-14 18:05:22 +0900

    feat : 공통예외핸들러, entity 추가

---
+++ src/main/java/com/example/newsfeedproject/common/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.example.newsfeedproject.common.exception.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    //공통예외처리
+}
