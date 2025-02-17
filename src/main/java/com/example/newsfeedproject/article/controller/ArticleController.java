b66d55c12c15c197a649c0c51bf38ca14a264a7b
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-02-17 12:06:23 +0900

    feat : 게시글 좋아요 추가, 취소 기능 추가

--- src/main/java/com/example/newsfeedproject/article/controller/ArticleController.java
+++ src/main/java/com/example/newsfeedproject/article/controller/ArticleController.java
@@ -1,9 +1,33 @@
 package com.example.newsfeedproject.article.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.newsfeedproject.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/articles")
 public class ArticleController {
+    private final ArticleService articleService;
     //게시글 CRUD
     //게시글 좋아요
+
+    //게시글 좋아요 등록
+    @PostMapping("/{articleId}/favorite")
+    public ResponseEntity<Void> addRecommendArticle(@PathVariable Long articleId, @SessionAttribute(name = "userId") Long userId) {
+
+        articleService.addRecommendArticle(articleId, userId);
+
+        return ResponseEntity.ok().build();
+    }
+
+    //게시글 좋아요 취소
+    @DeleteMapping("/{articleId}/favorite")
+    public ResponseEntity<Void> removeRecommendArticle(@PathVariable Long articleId, @SessionAttribute(name = "userId") Long userId) {
+
+        articleService.cancelRecommendArticle(articleId, userId);
+
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/example/newsfeedproject/recommand/repository/RecommendArticleRepository.java
+++ src/main/java/com/example/newsfeedproject/recommand/repository/RecommendArticleRepository.java
@@ -3,5 +3,10 @@ package com.example.newsfeedproject.recommand.repository;
 import com.example.newsfeedproject.recommand.entity.RecommendArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface RecommendArticleRepository extends JpaRepository<RecommendArticle, Long> {
+    Optional<RecommendArticle> findByArticleIdAndUserId(Long articleId, Long userId);
+    Long deleteRecommendArticleByArticleIdAndUserId(Long articleId, Long userId);
 }
