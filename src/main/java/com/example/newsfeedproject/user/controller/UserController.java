19823c0711225cd6ecb10eb08964065a8b3b0100
jhwang2 <ahkiler12@gmail.com>
2025-02-18 15:42:51 +0900

    feat: User Controller 의 findById 와 updateUser 기능 추가

--- src/main/java/com/example/newsfeedproject/user/controller/UserController.java
+++ src/main/java/com/example/newsfeedproject/user/controller/UserController.java
@@ -11,8 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 public class UserController {
