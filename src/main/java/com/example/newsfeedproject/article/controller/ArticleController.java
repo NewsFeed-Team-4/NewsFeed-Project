b654bda4533d81cb591af592544228275e69f20c
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-02-18 13:39:36 +0900

    fix : 인증정보 가져오는방식 수정

--- src/main/java/com/example/newsfeedproject/article/controller/ArticleController.java
+++ src/main/java/com/example/newsfeedproject/article/controller/ArticleController.java
@@ -1,6 +1,8 @@
 package com.example.newsfeedproject.article.controller;
 
 import com.example.newsfeedproject.article.service.ArticleService;
+import com.example.newsfeedproject.common.annotations.UserAuth;
+import com.example.newsfeedproject.common.annotations.UserAuthDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +17,18 @@ public class ArticleController {
 
     //게시글 좋아요 등록
     @PostMapping("/{articleId}/favorite")
-    public ResponseEntity<Void> addRecommendArticle(@PathVariable Long articleId, @SessionAttribute(name = "userId") Long userId) {
-
-        articleService.addRecommendArticle(articleId, userId);
+    public ResponseEntity<Void> addRecommendArticle(@PathVariable Long articleId,
+                                                    @UserAuth UserAuthDto userAuthDto) {
+        articleService.addRecommendArticle(articleId, userAuthDto.getUserId());
 
         return ResponseEntity.ok().build();
     }
 
     //게시글 좋아요 취소
     @DeleteMapping("/{articleId}/favorite")
-    public ResponseEntity<Void> removeRecommendArticle(@PathVariable Long articleId, @SessionAttribute(name = "userId") Long userId) {
-
-        articleService.cancelRecommendArticle(articleId, userId);
+    public ResponseEntity<Void> removeRecommendArticle(@PathVariable Long articleId,
+                                                       @UserAuth UserAuthDto userAuthDto) {
+        articleService.cancelRecommendArticle(articleId, userAuthDto.getUserId());
 
         return ResponseEntity.ok().build();
     }
